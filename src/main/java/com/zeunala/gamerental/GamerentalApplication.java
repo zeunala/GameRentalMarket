e570e9335c8de9bf73ab564217ef2b7d5d55456d
zeunala <zeunala@naver.com>
2022-12-30 19:42:34 +0900

    Create project

---
+++ src/main/java/com/zeunala/gamerental/GamerentalApplication.java
@@ -0,0 +1,13 @@
+package com.zeunala.gamerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeunala/gamerental/GamerentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeunala.gamerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
