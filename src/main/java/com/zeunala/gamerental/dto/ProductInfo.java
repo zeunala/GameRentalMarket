9aa5784be3e1fbe6b5d450cf52cdb3a9b2c34e4e
zeunala <zeunala@naver.com>
2023-01-21 18:46:35 +0900

    feat: 상품 관련 DTO 추가

---
+++ src/main/java/com/zeunala/gamerental/dto/ProductInfo.java
@@ -0,0 +1,33 @@
+package com.zeunala.gamerental.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class ProductInfo {
+    Integer id;
+    String title;
+    String description;
+    Integer originalPrice;
+    String filePath;
+    String createDate; // "2023-01-01"의 형태로 저장
+    Integer minRentalPrice;
+    Integer minUsedPrice;
+
+    public ProductInfo(Integer id, String title, String description, Integer originalPrice, String filePath,
+                       String createDate, Integer minRentalPrice, Integer minUsedPrice) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.originalPrice = originalPrice;
+        this.filePath = filePath;
+        this.createDate = createDate;
+        this.minRentalPrice = minRentalPrice;
+        this.minUsedPrice = minUsedPrice;
+    }
+}
