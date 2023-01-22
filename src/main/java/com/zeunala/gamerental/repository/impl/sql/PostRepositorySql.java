ac1d19d292193707878a9816c8751ec83c7edb27
zeunala <zeunala@naver.com>
2023-01-22 18:15:27 +0900

    refactor: SQL문 공통 부분 추출
    판매글 조회 SQL문의 공통되는 SELECT~FROM 부분을 추출하여 중복 제거

--- src/main/java/com/zeunala/gamerental/repository/impl/sql/PostRepositorySql.java
+++ src/main/java/com/zeunala/gamerental/repository/impl/sql/PostRepositorySql.java
@@ -4,7 +4,7 @@ public class PostRepositorySql {
     /**
      * FORMATDATETIME: H2 문법
      */
-    public static final String FIND_POST_INFO_BY_POST_ID = """
+    private static final String COMMON_SELECT_POST_INFO_SQL = """
             SELECT post.id AS id,
                 (SELECT users.nickname FROM users WHERE users.id = post.seller_users_id)
                     AS sellerUsersNickname,
@@ -17,44 +17,19 @@ public class PostRepositorySql {
             FROM post
                 INNER JOIN product ON post.product_id = product.id
                 INNER JOIN category ON product.category_id = category.id
-            WHERE post.id = :postId;
             """;
+    public static final String FIND_POST_INFO_BY_POST_ID =
+            COMMON_SELECT_POST_INFO_SQL + """
+                    WHERE post.id = :postId;
+                    """;
 
-    /**
-     * FORMATDATETIME: H2 문법
-     */
-    public static final String FIND_ALL_POST_INFO_BY_PRODUCT_ID_AND_RENTAL_FLAG_AND_STATUS = """
-            SELECT post.id AS id,
-                (SELECT users.nickname FROM users WHERE users.id = post.seller_users_id)
-                    AS sellerUsersNickname,
-                post.rental_flag AS rentalFlag, post.direct_flag AS directFlag,
-                category.name AS categoryName, product.title AS title,
-                post.price AS price, post.extension_price AS extensionPrice,
-                post.deposit AS deposit, post.comment AS comment,
-                FORMATDATETIME(post.create_date, 'yyyy-MM-dd') AS createDate,
-                post.status AS status
-            FROM post
-                INNER JOIN product ON post.product_id = product.id
-                INNER JOIN category ON product.category_id = category.id
-            WHERE post.product_id = :productId AND post.rental_flag = :rentalFlag AND post.status = :status;
-            """;
+    public static final String FIND_ALL_POST_INFO_BY_PRODUCT_ID_AND_RENTAL_FLAG_AND_STATUS =
+            COMMON_SELECT_POST_INFO_SQL + """
+                    WHERE post.product_id = :productId AND post.rental_flag = :rentalFlag AND post.status = :status;
+                    """;
 
-    /**
-     * FORMATDATETIME: H2 문법
-     */
-    public static final String FIND_ALL_POST_INFO_BY_SELLER_USERS_ID_AND_STATUS = """
-            SELECT post.id AS id,
-                (SELECT users.nickname FROM users WHERE users.id = post.seller_users_id)
-                    AS sellerUsersNickname,
-                post.rental_flag AS rentalFlag, post.direct_flag AS directFlag,
-                category.name AS categoryName, product.title AS title,
-                post.price AS price, post.extension_price AS extensionPrice,
-                post.deposit AS deposit, post.comment AS comment,
-                FORMATDATETIME(post.create_date, 'yyyy-MM-dd') AS createDate,
-                post.status AS status
-            FROM post
-                INNER JOIN product ON post.product_id = product.id
-                INNER JOIN category ON product.category_id = category.id
-            WHERE post.seller_users_id = :sellerUsersId AND post.status = :status;
-            """;
+    public static final String FIND_ALL_POST_INFO_BY_SELLER_USERS_ID_AND_STATUS =
+            COMMON_SELECT_POST_INFO_SQL + """
+                    WHERE post.seller_users_id = :sellerUsersId AND post.status = :status;
+                    """;
 }
