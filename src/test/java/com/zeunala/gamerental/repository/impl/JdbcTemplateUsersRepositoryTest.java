0a5fee8e6f79d48b0a451ff90e109af25fca48aa
zeunala <zeunala@naver.com>
2023-01-23 16:39:16 +0900

    test: Users 테이블에 새로운 데이터 추가 테스트 추가

--- src/test/java/com/zeunala/gamerental/repository/impl/JdbcTemplateUsersRepositoryTest.java
+++ src/test/java/com/zeunala/gamerental/repository/impl/JdbcTemplateUsersRepositoryTest.java
@@ -9,9 +9,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @Slf4j
 @SpringBootTest
 class JdbcTemplateUsersRepositoryTest {
@@ -33,5 +35,25 @@ class JdbcTemplateUsersRepositoryTest {
     void findByLoginId(String loginId) {
         Users users = usersRepository.findByLoginId(loginId);
         assertThat(users.getLoginId()).isEqualTo(loginId);
+    }
+
+    @Test
+    @DisplayName("새로운 유저 추가")
+    void save() {
+        Users users = new Users("무", "EEE", "EEE@gmail.com", "010-5555-5555",
+                "대전", "EEE258", "2580");
+        Users savedUsers = usersRepository.save(users);
+        log.info("추가된 유저 정보: {}", savedUsers);
+        assertThat(savedUsers.getId()).isNotNull();
+    }
+    
+    @Test
+    @DisplayName("새로운 유저를 추가하고 정보가 정상적으로 조회되는지 테스트")
+    void save_saveNewUserThenFind_isNotNull() {
+        Users users = new Users("기", "FFF", "FFF@gmail.com", "010-6666-6666",
+                "대구", "FFF147", "1470");
+        Users savedUsers = usersRepository.save(users);
+        Users findUsers = usersRepository.findByLoginId("FFF147");
+        assertThat(savedUsers).usingRecursiveComparison().isEqualTo(findUsers);
     }
 }
