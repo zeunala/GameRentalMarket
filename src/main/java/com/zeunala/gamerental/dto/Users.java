eb0650bb1cd1ec5225bc86c6a219ed5fbcc3f593
zeunala <zeunala@naver.com>
2023-01-23 15:55:17 +0900

    feat: Users 테이블 관련 DTO 추가

---
+++ src/main/java/com/zeunala/gamerental/dto/Users.java
@@ -0,0 +1,43 @@
+package com.zeunala.gamerental.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Users {
+    Integer id; // DB에서 생성할 PK값으로 생성자에 존재하지 않음
+    String name;
+    String nickname;
+    String email;
+    String tel;
+    String homeAddress;
+    String loginId;
+    String loginPassword;
+
+    public Users(String name, String nickname, String email, String tel,
+                 String homeAddress, String loginId, String loginPassword) {
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+        this.tel = tel;
+        this.homeAddress = homeAddress;
+        this.loginId = loginId;
+        this.loginPassword = loginPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", name='" + name.substring(0, name.length() / 2) + "**" + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", email='" + email.substring(0, email.length() / 2) + "*******" + '\'' +
+                ", tel='" + tel.substring(0, tel.length() / 2) + "*******" + '\'' +
+                ", homeAddress='" + homeAddress + '\'' +
+                ", loginId='" + loginId + '\'' +
+                '}';
+    }
+}
