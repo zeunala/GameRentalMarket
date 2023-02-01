071446aea1d9dd5788140d1f5b420c373d5ed9f0
zeunala <zeunala@naver.com>
2023-02-01 15:09:58 +0900

    feat: 로그인 페이지 빈 칸 입력시 에러 메시지 추가
    로그인 페이지에서 빈 상태로 입력시 에러 메시지도 뜨게 하고,
    추가로 스페이스만으로 이루어진 공백 입력시에도 에러가 발생하도록 한다.

--- src/main/java/com/zeunala/gamerental/dto/LoginForm.java
+++ src/main/java/com/zeunala/gamerental/dto/LoginForm.java
@@ -1,16 +1,16 @@
 package com.zeunala.gamerental.dto;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LoginForm {
-    @NotEmpty
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String loginId;
 
-    @NotEmpty
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String loginPassword;
 
     public LoginForm(String loginId, String loginPassword) {
