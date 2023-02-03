f8229475caa8d3aa5f152f46b3da139d673af393
zeunala <zeunala@naver.com>
2023-02-03 19:34:52 +0900

    feat: 상세 페이지 상품정보 로드 후 표시 기능 추가

--- src/main/java/com/zeunala/gamerental/controller/MainController.java
+++ src/main/java/com/zeunala/gamerental/controller/MainController.java
@@ -1,10 +1,18 @@
 package com.zeunala.gamerental.controller;
 
+import com.zeunala.gamerental.dto.ProductInfo;
+import com.zeunala.gamerental.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+    private final ProductService productService;
+
     @GetMapping("/")
     public String index() {
         return "redirect:main";
@@ -15,8 +23,15 @@ public class MainController {
         return "main";
     }
 
-    @GetMapping("/detail")
-    public String detail() {
+    @GetMapping("/detail/{productId}")
+    public String detail(@PathVariable Integer productId, Model model) {
+        ProductInfo productInfo = productService.getProductInfoByProductId(productId);
+
+        model.addAttribute("title", productInfo.getTitle());
+        model.addAttribute("imgUrl", "/" + productInfo.getFilePath());
+        model.addAttribute("minRentalPrice", productInfo.getMinRentalPrice());
+        model.addAttribute("minUsedPrice", productInfo.getMinUsedPrice());
+
         return "detail";
     }
 
