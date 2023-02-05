d5e03f43d630b6a4646d3cdba8523c3974c888ba
zeunala <zeunala@naver.com>
2023-02-05 21:15:01 +0900

    feat: 상세 페이지 하단 판매글 정보 Controller와 연결 추가

--- src/main/java/com/zeunala/gamerental/controller/MainController.java
+++ src/main/java/com/zeunala/gamerental/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.zeunala.gamerental.controller;
 
+import com.zeunala.gamerental.dto.PostInfo;
 import com.zeunala.gamerental.dto.ProductInfo;
+import com.zeunala.gamerental.service.PostService;
 import com.zeunala.gamerental.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,10 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MainController {
     private final ProductService productService;
+    private final PostService postService;
 
     @GetMapping("/")
     public String index() {
@@ -26,11 +31,16 @@ public class MainController {
     @GetMapping("/detail/{productId}")
     public String detail(@PathVariable Integer productId, Model model) {
         ProductInfo productInfo = productService.getProductInfoByProductId(productId);
+        List<PostInfo> rentalPosts = postService.getAllPostInfoByProductIdAndRentalFlagAndStatus(productId, 1, 0);
+        List<PostInfo> usedPosts = postService.getAllPostInfoByProductIdAndRentalFlagAndStatus(productId, 0, 0);
 
         model.addAttribute("title", productInfo.getTitle());
         model.addAttribute("imgUrl", "/" + productInfo.getFilePath());
         model.addAttribute("minRentalPrice", productInfo.getMinRentalPrice());
         model.addAttribute("minUsedPrice", productInfo.getMinUsedPrice());
+
+        model.addAttribute("rentalPosts", rentalPosts);
+        model.addAttribute("usedPosts", usedPosts);
 
         return "detail";
     }
