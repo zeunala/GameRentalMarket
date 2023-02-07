d0ad159be573edf08a58c5cdf6bedd1951853008
zeunala <zeunala@naver.com>
2023-02-08 02:12:08 +0900

    feat: 판매유형 선택시 그에 따른 입력 폼 변경 기능 추가
    추가로 입력 폼 변경시의 가독성을 위해 판매문구 행을 위로 이동함

--- src/main/resources/static/js/register_sell.js
+++ src/main/resources/static/js/register_sell.js
@@ -1,8 +1,44 @@
+const PostFormObj = {
+    _resetFormPrice() {
+        document.getElementById("price").value = null;
+        document.getElementById("extensionPrice").value = null;
+        document.getElementById("deposit").value = null;
+    },
+    selectRental() {
+        this._resetFormPrice();
+        document.getElementById("priceRow").classList.remove("d-none");
+
+        document.getElementById("extensionPriceRow").classList.remove("d-none");
+        document.getElementById("depositRow").classList.remove("d-none");
+        document.getElementById("rentalPriceHeader").classList.remove("d-none");
+        document.getElementById("usedPriceHeader").classList.add("d-none");
+    },
+    selectUsed() {
+        this._resetFormPrice();
+        document.getElementById("priceRow").classList.remove("d-none");
+
+        document.getElementById("extensionPriceRow").classList.add("d-none");
+        document.getElementById("depositRow").classList.add("d-none");
+        document.getElementById("rentalPriceHeader").classList.add("d-none");
+        document.getElementById("usedPriceHeader").classList.remove("d-none");
+    }
+}
+
 const EventObj = {
     /**
      * EventListener들을 등록
      */
     setEventListeners() {
+        /**
+         * 렌탈/중고 선택시 그에 따른 입력 폼 변경
+         */
+        document.getElementById("selectRental").addEventListener("change", (evt) => {
+            PostFormObj.selectRental();
+        });
+        document.getElementById("selectUsed").addEventListener("change", (evt) => {
+            PostFormObj.selectUsed();
+        });
+
         /**
          * 연장 허용안함/보증금 없음 체크시 입력창 비활성화
          */
