537bff560daafc80d8cf58352c24c8e9998fdc68
zeunala <zeunala@naver.com>
2023-02-12 18:05:30 +0900

    feat: 거래 당사자 외 거래진행글 접근 불가 기능 추가

--- src/main/java/com/zeunala/gamerental/controller/DealController.java
+++ src/main/java/com/zeunala/gamerental/controller/DealController.java
@@ -4,6 +4,7 @@ import com.zeunala.gamerental.dto.DealInfo;
 import com.zeunala.gamerental.dto.Users;
 import com.zeunala.gamerental.service.DealService;
 import com.zeunala.gamerental.service.UsersService;
+import com.zeunala.gamerental.util.SessionName;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,9 +23,15 @@ public class DealController {
     private final UsersService usersService;
 
     @GetMapping("/buy/{dealId}")
-    public String dealBuy(@PathVariable Integer dealId, Model model) {
+    public String dealBuy(@PathVariable Integer dealId, HttpSession session, Model model) {
         DealInfo dealInfo = dealService.getDealInfoByDealId(dealId);
         Users seller = usersService.getUsersByNickname(dealInfo.getSellerUsersNickname());
+
+        // 구매자가 아닌 경우 접근 불가
+        Integer buyerId = usersService.getUsersByNickname(dealInfo.getBuyerUsersNickname()).getId();
+        if (!buyerId.equals((Integer) session.getAttribute(SessionName.LOGIN_USERS_ID))) {
+            return "redirect:/main";
+        }
 
         model.addAttribute("deal", dealInfo);
         model.addAttribute("seller", seller);
@@ -32,9 +39,15 @@ public class DealController {
     }
 
     @GetMapping("/sell/{dealId}")
-    public String dealSell(@PathVariable Integer dealId, Model model) {
+    public String dealSell(@PathVariable Integer dealId, HttpSession session, Model model) {
         DealInfo dealInfo = dealService.getDealInfoByDealId(dealId);
         Users buyer = usersService.getUsersByNickname(dealInfo.getBuyerUsersNickname());
+
+        // 판매자가 아닌 경우 접근 불가
+        Integer sellerId = usersService.getUsersByNickname(dealInfo.getSellerUsersNickname()).getId();
+        if (!sellerId.equals((Integer) session.getAttribute(SessionName.LOGIN_USERS_ID))) {
+            return "redirect:/main";
+        }
 
         model.addAttribute("deal", dealInfo);
         model.addAttribute("buyer", buyer);
