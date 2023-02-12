3ef96536e9c593027669082873442f9572ca6a3e
zeunala <zeunala@naver.com>
2023-02-12 16:40:38 +0900

    feat: 거래글 판매 진행 페이지 및 정보 불러오기 기능 추가
    판매 진행 정보를 확인할 수 있는 페이지 생성
    마이페이지 판매 목록에서 접근 가능하도록 함
    URL을 통해 판매 당사자 외 접근 못하도록 추후 조치 필요

---
+++ src/main/java/com/zeunala/gamerental/controller/DealController.java
@@ -0,0 +1,38 @@
+package com.zeunala.gamerental.controller;
+
+import com.zeunala.gamerental.dto.DealInfo;
+import com.zeunala.gamerental.dto.Users;
+import com.zeunala.gamerental.service.DealService;
+import com.zeunala.gamerental.service.UsersService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(path = "/deal")
+@Slf4j
+public class DealController {
+    private final DealService dealService;
+    private final UsersService usersService;
+
+    @GetMapping("/buy/{dealId}")
+    public String dealBuy(@PathVariable Integer dealId, Model model) {
+        return "deal_buy";
+    }
+
+    @GetMapping("/sell/{dealId}")
+    public String dealSell(@PathVariable Integer dealId, Model model) {
+        DealInfo dealInfo = dealService.getDealInfoByDealId(dealId);
+        Users buyer = usersService.getUsersByNickname(dealInfo.getBuyerUsersNickname());
+
+        model.addAttribute("deal", dealInfo);
+        model.addAttribute("buyer", buyer);
+        return "deal_sell";
+    }
+}
