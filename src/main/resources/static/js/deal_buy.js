11fd555ee171d67b96c703dcb205ce4f2f519ab3
zeunala <zeunala@naver.com>
2023-02-14 01:41:12 +0900

    feat: 판매/구매 진행 페이지에서 거래글 삭제 기능 추가

---
+++ src/main/resources/static/js/deal_buy.js
@@ -0,0 +1,43 @@
+const PostObj = {
+    /**
+     * alert창 확인 선택시 현재 페이지의 deal을 삭제한다.
+     */
+    deleteDeal() {
+        // deal/buy/{dealId}에서 dealId를 취한다.
+        const dealId = window.location.pathname.split("/")[3];
+        if (confirm("정말로 현재 거래를 취소하시겠습니까? 삭제 후에는 복구할 수 없습니다.")) {
+            fetch(`/deal/${dealId}`, {
+                method: "DELETE",
+            }).then(() => {
+                window.location.href = "/mypage/buy";
+            });
+        }
+    }
+}
+
+const EventObj = {
+    /**
+     * EventListener들을 등록
+     */
+    setEventListeners() {
+        /**
+         * 글 삭제 버튼을 누르면 확인창을 띄우고, 확인을 누르면 해당 판매글이 삭제된다.
+         */
+        document.querySelectorAll(".deal-delete").forEach(e => {
+            e.addEventListener("click", () => {
+                PostObj.deleteDeal();
+            });
+        });
+    }
+}
+
+/**
+ * 문서 로딩 후 실행될 함수들
+ */
+function initConfig() {
+    EventObj.setEventListeners();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    initConfig();
+});
