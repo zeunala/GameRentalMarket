b00d60cf9af355cf122c11be92960756233b8da9
zeunala <zeunala@naver.com>
2023-05-17 14:19:40 +0900

    perf: 프로모션 관련 정보 조회 성능 개선
    Promotion 엔티티 조회 후 DTO 변환 시의 N+1문제 해결

--- src/main/java/com/zeunala/gamerental/repository/jpa/SpringDataJpaPromotionRepository.java
+++ src/main/java/com/zeunala/gamerental/repository/jpa/SpringDataJpaPromotionRepository.java
@@ -1,7 +1,13 @@
 package com.zeunala.gamerental.repository.jpa;
 
 import com.zeunala.gamerental.domain.Promotion;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SpringDataJpaPromotionRepository extends JpaRepository<Promotion, Integer> {
+    @Override
+    @EntityGraph(attributePaths = {"file"})
+    List<Promotion> findAll();
 }
